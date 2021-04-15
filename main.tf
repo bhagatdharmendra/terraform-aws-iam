414ebc71285cce0fcd7ce5853d286f33ecb0dcf7
Dharmendra Bhagat <bhagatdharmendra0@gmail.com>
2021-04-15 12:41:27 +0530

    terraform cloud

--- main.tf
+++ main.tf
@@ -1,190 +1,62 @@
 provider "aws" {
-  region     = "ap-south-1"
-   profile = "sonu-terraform"
+  access_key = var.access_key 
+  secret_key = var.secret_key
 }
-
-resource "aws_vpc" "main" {
-  cidr_block       = var.vpc_cidr
-  instance_tenancy = "default"
-  enable_dns_hostnames = true
+resource "aws_iam_user" "newuser" {
+  count = length(var.newuser)
+  name = element(var.newuser, count.index)
+  path = "/system/"
+  force_destroy = true
 
   tags = {
-    Name = "sonu-vpc"
+    Name = element(var.newuser, count.index)
   }
 }
 
-resource "aws_subnet" "subnet-1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "192.168.0.0/24"
-  availability_zone = "ap-south-1a"
-  tags = {
-    Name = "public-sonu-vpc-subnet"
-  }
+
+resource "aws_iam_access_key" "test" {
+  count = length(var.newuser)
+  user = element(var.newuser, count.index)
+}
+resource "aws_iam_user_login_profile" "u" {
+  count = length(var.newuser)
+  user                    = element(var.newuser, count.index)
+  password_reset_required = true
+  pgp_key="keybase:terraform_user"
 }
 
-resource "aws_subnet" "subnet-2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "192.168.1.0/24"
-  availability_zone = "ap-south-1b"
-  tags = {
-    Name = "private-sonu-vpc-subnet"
-  }
+resource "aws_iam_policy" "policy" {
+  name        = "ec2-full-terraform"
+  path        = "/"
+  description = "My test policy"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "ec2:*",
+            "Resource": "*",
+            "Effect": "Allow",
+            "Condition": {
+                "StringEquals": {
+                    "ec2:Region": "ap-south-1"
+                }
+            }
+        }
+    ]
 }
-######### public Security Group 
-resource "aws_security_group" "allow_tls" {
-  name        = "public_sonu_vpc"
-  description = "ssh,http"
-  vpc_id      = aws_vpc.main.id
+  EOF
 
-  dynamic "ingress" {
-    for_each = var.allports
-    iterator = port
-    content{
-    from_port   = port.value
-    to_port     = port.value
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-  
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "public_sonu_vpc_SG"
-  }
 }
-
-######### private Security Group 
-resource "aws_security_group" "allow_tls2" {
-  name        = "private_sonu_vpc"
-  description = "ssh,http for private access only "
-  vpc_id      = aws_vpc.main.id
-
-   dynamic "ingress" {
-    for_each = var.allprivate_ports
-   iterator = port
-    content{
-    
-    from_port   = port.value
-    to_port     = port.value
-    protocol    = "tcp"
-    security_groups = [ aws_security_group.allow_tls.id ]
-    }
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "private_sonu_vpc_SG"
-  }
-  
-}
-##### Internet Gateways for public instance ##
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "sonu-vpc-gateways"
-  }
-}
-##### routing tables #######
-resource "aws_route_table" "r" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-   tags = {
-    Name = "public_routing_tables"
-  }
+resource "aws_iam_user_policy_attachment" "test-attach" {
   depends_on = [
-    aws_internet_gateway.gw
+     aws_iam_policy.policy, aws_iam_access_key.test
   ]
+  count = length(var.newuser)
+  user       = element(var.newuser, count.index)
+  policy_arn = aws_iam_policy.policy.arn
 }
 
-######## public subnet association with routing table  ###
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.subnet-1.id
-  route_table_id = aws_route_table.r.id
-  
-  depends_on = [
-    aws_subnet.subnet-1
-       
-  ]
-
-}
-
-
- 
-######### Launch EC2-instance  in public subnet  ######  
-
-resource "aws_instance" "instance1" {
-  ami           = "ami-0732b62d310b80e97"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [ aws_security_group.allow_tls.id ]
-  associate_public_ip_address = true
-  subnet_id = aws_subnet.subnet-1.id
-  key_name = "bhagat-laptopkey"
-  
-  
-  
-  tags = {
-    Name = "wordpress-public"
-  }
- 
-}
-
-output "ip_of_instnace" {
-  value = aws_instance.instance1.public_ip
-}
-
-
-
-locals {
-  ingres_rules = [{
-   port = 80
-   description = "port 80 allow"
-  },
-  {
-    port = 443
-   description = "port 443 allow"
-  },
-   {
-    port = 22
-   description = "port 22 allow"
-  },
-   {
-   port = 9090
-   description = "port 22 allow"
-  }
-  ]
-}
-resource "aws_security_group" "allow_tls_ext" {
-  name        = "test_sg"
-  vpc_id      = aws_vpc.main.id
-  dynamic "ingress" {
-    for_each = local.ingres_rules
-    content {
-      description = ingress.value.description
-      from_port = ingress.value.port
-      to_port = ingress.value.port
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-  tags = {
-    "Name" = "test_sg"
-  }
+output "password" {
+value= aws_iam_user_login_profile.u[*].encrypted_password
 }
--- variable.tf
+++ variable.tf
@@ -1,16 +1,5 @@
-variable "allports" {
-    type = list(number)
-    default = [22,443,80]
-  
-}
-
-variable "allprivate_ports" {
-    type = list(number)
-    default = [80,443,22,3306]
-    description = "private-subnet-only"
-  
-}
-variable "vpc_cidr" {
-  type = string
+variable "newuser" {
+    type = list
+    default = ["sonu","jack","jone"]
   
 }
